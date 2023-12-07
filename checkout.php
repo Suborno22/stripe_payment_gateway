00a6a17dda5399b7d797cdb2725fc1c741da1ae0
Suborno <112712723+Suborno22@users.noreply.github.com>
2023-12-07 13:26:31 +0530

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -14,8 +14,8 @@ $quantity = isset($_GET['quantity']) ? $_GET['quantity'] : 1;
 // Create the checkout session
 $checkout_session = \Stripe\Checkout\Session::create([
    "mode" => "payment",
-   "success_url" => "http://stripe_payment_gateway.dvl.to/success.php",
-   "cancel_url" => "http://stripe_payment_gateway.dvl.to/index.html",
+   "success_url" => "http://localhost/stripe_payment_gateway/success.php",
+   "cancel_url" => "http://localhost/stripe_payment_gateway/index.html",
    "line_items" => [
        [
            "quantity" => $quantity,
